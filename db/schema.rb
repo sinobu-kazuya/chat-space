4a15db976bc7cbcda2eb9a225eb02138c72c8534
信夫知也 <kazooie.nobuo@gmail.com>
2018-08-10 14:08:17 +0900

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -28,13 +28,6 @@ ActiveRecord::Schema.define(version: 20180810030327) do
     t.index ["name"], name: "index_groups_on_name", using: :btree
   end
 
-  create_table "gruops", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",       null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_gruops_on_name", using: :btree
-  end
-
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                                null: false
     t.string   "email",                  default: "", null: false
