4e96cb883d94dff1d7f411a9de191d9210bd8fe2
信夫知也 <kazooie.nobuo@gmail.com>
2018-08-18 20:17:49 +0900

    Add  interval function and something

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,8 @@
-$(function() {
+
+$(document).on('turbolinks:load',function() {
 function buildHTML(message){
   var img = message.image ? `<img src= ${message.image}>` : "";
-  var html = `<div class="chatspace__list--message">
+  var html = `<div class="chatspace__list--message" data-id=${ message.id }>
                 <div class="chatspace__list--message-indent">
                   <div class="chatspace__list--message-indent-name">
                     ${message.user_name}
@@ -20,8 +21,8 @@ function buildHTML(message){
   return html;
 }
 
-function scroll(messagebox){
-   messagebox.animate({scrollTop: $('.chatspace__list')[0].scrollHeight},'fast');
+function scroll(){
+   $('.chatspace__list').animate({scrollTop: $('.chatspace__list')[0].scrollHeight},'fast');
  }
 
   $('#new_message').on('submit', function(e){
@@ -37,8 +38,8 @@ function scroll(messagebox){
       contentType: false
     })
     .done(function(data) {
-      var html = buildHTML(data)
-      $('.chatspace__list').append(html)
+      var html = buildHTML(data);
+      $('.chatspace__list').append(html);
       $('.chatspace__fotter--search-bottum').prop('disabled', false);
       scroll($('.chatspace__list'))
       $('#new_message')[0].reset();
@@ -47,4 +48,30 @@ function scroll(messagebox){
       alert('error');
     })
   })
+
+  var interval = setInterval(function() {
+    console.log('ok');
+    if (window.location.pathname.match(/\/groups\/\d+\/messages/)) {
+        var messageId = $('.chatspace__list--message:last').data('id');
+      $.ajax({
+        url: location.href,
+        type: 'GET',
+        data: {id: messageId},
+        dataType: 'json'
+      })
+      .done(function(data) {
+        var html = '';
+        data.forEach(function(data) {
+          html = buildHTML(data);
+        });
+        $('.chatspace__list').append(html);
+        scroll();
+      })
+      .fail(function(){
+        alert('error');
+      })
+    } else{
+      clearInterval(interval);
+    }
+  }, 5000);
 })
